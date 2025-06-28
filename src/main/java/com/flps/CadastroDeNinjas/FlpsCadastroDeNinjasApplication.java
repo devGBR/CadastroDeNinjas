0738b32dc1062fccab36c8d037200be9122c5dd1
devgbr <devgbr47@gmail.com>
2025-06-28 11:28:25 -0300

    first commit

---
+++ src/main/java/com/flps/CadastroDeNinjas/FlpsCadastroDeNinjasApplication.java
@@ -0,0 +1,13 @@
+package com.flps.CadastroDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlpsCadastroDeNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlpsCadastroDeNinjasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flps/CadastroDeNinjas/FlpsCadastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flps.CadastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlpsCadastroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
